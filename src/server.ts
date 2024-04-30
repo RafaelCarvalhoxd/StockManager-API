b680b7f61307a7e5ce8ba2e383491e209da57357
Faelzxd <devrafaelcarvalho14@gmail.com>
2024-04-30 19:05:10 -0300

    feat: add server

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,15 @@
-console.log('Hello, World!')
+
+import { ApiExpress } from "./api/express/api.express";
+import { CategoryRoute } from "./routes/category.route";
+import { ProductsRoutes } from "./routes/product.route";
+
+const api = ApiExpress.build();
+
+const categoryRoute = CategoryRoute.build();
+const productsRoute = ProductsRoutes.build();
+
+api.app.use("/categories", categoryRoute.router);
+api.app.use("/products", productsRoute.router);
+
+api.start(3000);
+
