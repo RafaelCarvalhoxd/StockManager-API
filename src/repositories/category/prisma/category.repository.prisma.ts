f0665b350ab4ac4619f81e069ad85fccdd0e4b0e
Faelzxd <devrafaelcarvalho14@gmail.com>
2024-04-29 23:07:52 -0300

    feat: add prisma category repository

---
+++ src/repositories/category/prisma/category.repository.prisma.ts
@@ -0,0 +1,87 @@
+import { PrismaClient } from "@prisma/client";
+import { CategoryRepository } from "../category.repository";
+import { Category } from "../../../entities/category/category";
+
+export class CategoryRepositoryPrisma implements CategoryRepository {
+    private constructor(readonly prisma: PrismaClient) {}
+
+    public static build(prisma: PrismaClient) {
+        return new CategoryRepositoryPrisma(prisma);
+    }
+
+    public async save(category: Category): Promise<void> {
+        const data = {
+            id: category.id,
+            name: category.name
+        }
+
+        await this.prisma.category.create({
+            data
+        });
+    }
+
+    public async list(): Promise<Category[]> {
+       const aCategories = await this.prisma.category.findMany();
+
+        const categories: Category[] = aCategories.map(category => {
+            return Category.with(category.id, category.name);
+        });
+
+        return categories;
+    }
+
+    public async update(category: Category): Promise<void> {
+        const aCategory = await this.prisma.category.findUnique({
+            where: {
+                id: category.id
+            }
+        });
+
+        if (!aCategory) {
+            throw new Error('Category not found');
+        }
+
+        const data = {
+            name: category.name
+        }
+
+        await this.prisma.category.update({
+            where: {
+                id: category.id
+            },
+            data
+        });
+    }
+
+    public async find(id: string): Promise<Category | undefined> {
+        const aCategory = await this.prisma.category.findUnique({
+            where: {
+                id: id
+            }
+        });
+
+        if (!aCategory) {
+            return undefined;
+        }
+
+        return Category.with(aCategory.id, aCategory.name);
+    }
+
+    public async delete(id: string): Promise<void> {
+        const aCategory = await this.prisma.category.findUnique({
+            where: {
+                id: id
+            }
+        });
+
+        if (!aCategory) {
+            throw new Error('Category not found');
+        }
+
+        await this.prisma.category.delete({
+            where: {
+                id: id
+            }
+        });
+    }
+}
