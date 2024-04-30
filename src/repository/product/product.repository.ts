1c14c45dcb37195569fd4975a5c141b07c0ca56e
Rafael Carvalho <156271159+RafaelCarvalhoxd@users.noreply.github.com>
2024-04-30 12:29:50 -0300

    Delete src/repository directory

--- src/repository/product/product.repository.ts
+++
@@ -1,9 +0,0 @@
-import { Product } from "../../entities/product/product";
-
-export interface ProductRepository {
-    save(product: Product): Promise<void>; 
-    list(): Promise<Product[]>; 
-    find(id: string): Promise<Product | null>;
-    update(product: Product): Promise<void>;
-    delete(id: string): Promise<void>;
-}
